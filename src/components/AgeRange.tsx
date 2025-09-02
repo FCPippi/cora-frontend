9ad7ab2cd310f192f966fac98618e91bced850cc
Gustavo Trevisol <gustavotrevisol@MacBook-Air-de-Gustavo-4.local>
2025-09-01 21:43:34 -0300

    creation component of age range filter

---
+++ src/components/AgeRange.tsx
@@ -0,0 +1,30 @@
+import "../styles/AgeRangeStyle.css";
+import React from "react";
+
+
+function AgeRange(){
+    return(
+        <div className="FaixaEtaria">
+            
+            
+            <div className="filtro">
+            <label className="texto" htmlFor= "filtro1">
+                9 - 12 anos
+                <input className="checkbox" type="checkbox" id="filtro1"/>
+            </label>
+            </div>
+           
+           
+           <div className="filtro">
+            <label className="texto" htmlFor="filtro2">
+                13 - 15 anos
+                <input className="checkbox" type="checkbox" id="filtro2"/>
+            </label>
+            </div>
+
+            
+        </div>
+    )
+    }
+
+export default AgeRange
