4bde144f6a0bb9e768c1049a3f42067042cfd7be
DudaWendelMaia <dudawendelmaia@gmail.com>
2025-09-03 01:08:30 -0300

    feat: added search bar component

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,15 @@
 import CoraLogoIcon from "./icons/CoraLogoIcon"
 import AgeRange from "./components/AgeRange"
+import SearchBar from "./components/SearchBar"
 
 function App() {
 
   return (
     <>
       <CoraLogoIcon />
+      <SearchBar />    
       <AgeRange/>
       <h1>Main page</h1>
-      
     </>
 
 
--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-<<<<<<< HEAD:cora-frontend/src/main.tsx
 //import './index.css'
-=======
->>>>>>> dev:src/main.tsx
 import App from './App.tsx'
 
 createRoot(document.getElementById('root')!).render(
