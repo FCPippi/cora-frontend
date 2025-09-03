9d2377d1df9beb72f296e1a079dc337292ee4398
DudaWendelMaia <dudawendelmaia@gmail.com>
2025-09-03 01:29:29 -0300

    fix: search bar component

--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -1,23 +1,12 @@
 import React from "react";
 import styles from "../styles/SearchBar.module.css";
+import SearchIcon from "../icons/SearchIcon";
 
 const SearchBar: React.FC = () => {
   return (
     <div className={styles.searchBarContainer}>
       <span className={styles.icon}>
-        {/* SVG de lupa */}
-        <svg width="28" height="28" viewBox="0 0 28 28" fill="none">
-          <circle cx="13" cy="13" r="9.5" stroke="#757575" strokeWidth="2" />
-          <line
-            x1="21.0607"
-            y1="21.3536"
-            x2="18"
-            y2="18.2929"
-            stroke="#757575"
-            strokeWidth="2"
-            strokeLinecap="round"
-          />
-        </svg>
+        <SearchIcon/>
       </span>
       <input
         className={styles.input}
