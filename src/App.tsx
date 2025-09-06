299afabfaab2fba2ea38cdbafa24a5b6c07d08ec
francisco.pippi <f.pippi@edu.pucrs.br>
2025-09-06 20:47:02 -0300

    add login page

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,34 @@
 import { Navbar } from "./components/Navbar/Navbar"
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route, Outlet } from "react-router-dom";
+import Login from "./pages/Login";
+
+const Layout = () => {
+  return (
+    <>
+      <Navbar />
+      <Outlet />
+    </>
+  );
+};
 
 function App() {
-
   return (
     <Router>
-    <Navbar/>
-      {/* <CoraLogoIcon /> */}
-       <Routes>
-        <Route path="/conta" element={<div>Página da Conta</div>} />
-        <Route path="/conteudos" element={<div>Página de Conteúdos</div>} />
-        <Route path="/forum" element={<div>Página do Fórum</div>} />
-        <Route path="/especialistas" element={<div>Página dos Especialistas</div>} />
-        <Route path="*" element={<div>Página Inicial</div>} />
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/login" element={<Login />} />
+        
+        <Route element={<Layout />}>
+          <Route path="/conta" element={<div style={{ padding: "2rem", textAlign: "center" }}>Página da Conta</div>} />
+          <Route path="/conteudos" element={<div style={{ padding: "2rem", textAlign: "center" }}>Página de Conteúdos</div>} />
+          <Route path="/forum" element={<div style={{ padding: "2rem", textAlign: "center" }}>Página do Fórum</div>} />
+          <Route path="/especialistas" element={<div style={{ padding: "2rem", textAlign: "center" }}>Página dos Especialistas</div>} />
+        </Route>
+        
+        <Route path="*" element={<div style={{ padding: "2rem", textAlign: "center" }}>Página não encontrada</div>} />
       </Routes>
     </Router>
-  )
+  );
 }
 
 export default App
