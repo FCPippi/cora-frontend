40dad8df7d014060a62148129ba3b99809f9fa05
Enzo Vivian <enzo.zortea@edu.pucrs.br>
2025-09-07 20:55:43 -0300

    feat: added upload icon to thumbnail upload

--- src/components/FileUpload.tsx
+++ src/components/FileUpload.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import "../styles/FileUpload.css";
-
+import UploadIcon from "../icons/UploadIcon";  
 interface FileUploadProps {
   onChange: React.ChangeEventHandler<HTMLInputElement>;
 }
@@ -15,18 +15,7 @@ const FileUpload: React.FC<FileUploadProps> = ({ onChange }) => {
         onChange={onChange}
       />
       <label htmlFor="fileInput" className="custom-button">
-        {/* SVG do botão */}
-        {/* <svg width="20" height="20" viewBox="0 0 24 24">
-          <path
-            d="M12 2L12 16M5 9L12 2L19 9"
-            stroke="#fff"
-            strokeWidth="2"
-            strokeLinecap="round"
-            strokeLinejoin="round"
-          />
-          <rect x="4" y="16" width="16" height="6" rx="2" fill="#555" />
-        </svg> */}
-        <span>Escolher arquivo</span>
+        <UploadIcon />
       </label>
     </div>
   );
