32c55b77f66ee2f3a2710df049f4be15d49c089e
FCPippi <francisco.pippi@edu.pucrs.br>
2025-09-10 02:44:48 -0300

    fix vercel deploy

--- src/components/Popup.tsx
+++ src/components/Popup.tsx
@@ -1,5 +1,5 @@
 import { useState, useEffect, useRef } from "react";
-import "../styles/Popup.css";
+import "../styles/popup.css";
 import AgeRange from "./AgeRange";
 import FilterIcon from "../icons/FilterIcon";
 
